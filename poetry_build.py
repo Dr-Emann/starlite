83d233cd2c29581828748d2f4406ab3b08652524
Zachary Dremann <dremann@gmail.com>
2022-07-17 19:25:23 -0400

    Move rust extension

--- poetry_build.py
+++ poetry_build.py
@@ -8,7 +8,7 @@ def build(setup_kwargs: Dict[str, Any]) -> None:
     Add rust_extensions to the setup dict
     """
     setup_kwargs["rust_extensions"] = [
-        RustExtension("starlite.rust_backend", path="rust_backend/Cargo.toml", binding=Binding.PyO3, optional=True)
+        RustExtension("starlite.rust_backend", path="extensions/rust/Cargo.toml", binding=Binding.PyO3, optional=True)
     ]
     setup_kwargs["zip_safe"] = False
 
